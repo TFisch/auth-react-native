f3eab8ce6024b1c831e311053f0c1c72d47a49df
Tim Fischer <emailtimfischer@gmail.com>
2018-09-30 00:40:33 -0600

    Add password entry

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
-  state = { email: '' };
+  state = {
+    email: '',
+    password: ''
+  };
 
   render() {
+    const { email, password } = this.state;
     return (
       <Card>
         <CardSection >
           <Input
             label="Email"
             placeholder="user@gmail.com"
-            value={this.state.email}
+            value={email}
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            value={password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
         <CardSection>
           <Button>
             Log In
