9fae3f2ab33593288b5c44cdd61aaead814ebdb4
Tim Fischer <emailtimfischer@gmail.com>
2018-09-30 00:09:48 -0600

    cover

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import LoginForm from './components/LoginForm'
 class App extends Component {
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyBjYwADQQbS-V_COX_l8t70jkY1F3K8apo",
       authDomain: "auth-eec58.firebaseapp.com",
       databaseURL: "https://auth-eec58.firebaseio.com",
       projectId: "auth-eec58",
