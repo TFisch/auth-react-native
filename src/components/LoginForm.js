ac35cf0b645d407104f3d988b4afdc5738ceb940
Tim Fischer <emailtimfischer@gmail.com>
2018-09-29 23:32:03 -0600

    Create Input component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,7 +12,8 @@ class LoginForm extends Component {
           <TextInput
             value={this.state.text}
             onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }} />
+            style={{ height: 20, width: 100 }}
+          />
         </CardSection>
         <CardSection />
         <CardSection>
---
+++ src/components/common/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const input = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export { Input };
